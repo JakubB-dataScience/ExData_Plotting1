96d5b01b2aa2dad79da136f53a313fc6b5a0fe21
JakubB-dataScience <jakub.bizan@hotmail.com>
2021-11-01 17:41:49 +0100

    code and plots

---
+++ plot1.R
@@ -0,0 +1,41 @@
+# download, save and unzip file
+filename <- "exdata_data_household_power_consumption.zip"
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+if (!file.exists("household_power_consumption")) { 
+  unzip(filename) 
+}
+
+# table with Na value = ?, sep = ;
+consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# Date: Date in format dd/mm/yyyy
+consumption$Date <- as.Date(consumption$Date, "%d/%m/%Y")
+
+# data from the dates 2007-02-01 and 2007-02-02
+consumption <- subset(consumption,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+# only complete cases
+consumption <- consumption[complete.cases(consumption),]
+
+# time combining 
+dateTime <- paste(consumption$Date, consumption$Time)
+
+# creating vector date with time
+dateTime2 <- setNames(dateTime, "DateTime")
+
+# time standardization
+consumption <- consumption[ ,!(names(consumption) %in% c("Date","Time"))]
+consumption <- cbind(dateTime2, consumption)
+consumption$dateTime2 <- as.POSIXct(dateTime2)
+
+# plot 1
+hist(consumption$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+# save as PNG
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
